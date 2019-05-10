d068f3fbba04221a5e6e9d3428088b1397f9f78e
Artem Chivchalov <chivchalov@gmail.com>
2019-05-10 11:44:11 +0300

    feat(demo): add support of samples in room history format

--- demo/src/index.jsx
+++ demo/src/index.jsx
@@ -2,9 +2,73 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import terrain from './samples/terrain1.json';
-import samples from './samples/room1';
+import samples from './samples/1';
+import _ from 'lodash';
+
+function arraysToObject(obj) {
+    for(var key in obj) {
+        if(_.isArray(obj[key])) {
+            var result = {};
+            for(var i=0; i<obj[key].length; i++) {
+                result[i] = obj[key][i];
+            }
+            obj[key] = result;
+        }
+    }
+}
+
+function applyDiff(objects, diff) {
+    for (var id in diff) {
+        var objDiff = diff[id];
+        var obj = _.find(objects, {_id: id});
+        if(obj) {
+            if(objDiff !== null) {
+                arraysToObject(obj);
+                arraysToObject(objDiff);
+                obj = _.merge(obj, objDiff, (a,b)=>{
+                    if (_.isArray(a) && _.isArray(b)) {
+                        return b;
+                    }
+                });
+            }
+            else {
+                _.remove(objects, {_id: id});
+            }
+        }
+        else if(objDiff) {
+            obj = _.cloneDeep(objDiff);
+            objects.push(obj);
+        }
+    }
+}
 
 function start(_terrain, _samples) {
+
+    if(_samples.ticks) {
+        let newSamples = [], objects = [], users = {};
+        for(var i in _samples.ticks) {
+            applyDiff(objects, _samples.ticks[i]);
+            objects.forEach(object => {
+                if(object.user && !users[object.user]) {
+                    users[object.user] = {
+                        "_id": object.user,
+                        "username": "Screeps"
+                    }
+                }
+            });
+            newSamples.push({
+                gameTime: +i,
+                info: {},
+                flags: [],
+                visual: "",
+                users,
+                objects: _.cloneDeep(objects)
+            });
+        }
+        _samples = newSamples;
+    }
+
+
     ReactDOM.render(
         <App samples={_samples} terrain={_terrain} />,
         document.getElementById('root'),
