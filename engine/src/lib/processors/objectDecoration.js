c078bfffd89f896fb85331afcc84cacf8b9a9bca
Nikolay Kharitonov <o4kapuk@o4kapuk.ru>
2026-05-26 22:39:23 +0000

    🐛 fix controller decorations

--- engine/src/lib/processors/objectDecoration.js
+++ engine/src/lib/processors/objectDecoration.js
@@ -51,7 +51,6 @@ export default (params) => {
                 width: i.width,
                 height: i.height,
                 anchor: { x: 0.5, y: 0.5 },
-                parentLayer: layers.objects,
                 zIndex: 1,
             });
             if (i[graphic.alpha] !== undefined) {
